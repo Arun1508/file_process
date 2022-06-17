535d73d74edf86d16233f4479718a82709326b74
Arun1508 <arunmukesh.s@gmail.com>
2022-06-17 22:32:08 +0530

    adding cors header

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import HTMLResponse
 import xmltodict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/files/")
