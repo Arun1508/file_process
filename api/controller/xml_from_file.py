0c1e4ab44897ae0f29b5c192a4a4fefcf5010232
Arun1508 <arunmukesh.s@gmail.com>
2022-07-18 22:41:59 +0530

    Changes for log write

--- api/controller/xml_from_file.py
+++ api/controller/xml_from_file.py
@@ -1,9 +1,11 @@
+from email import message
 import os 
 import glob
 import json
 import asyncio
 from datetime import datetime
 import time
+import shutil
 
 import xmltodict
 import aiofiles
@@ -11,16 +13,61 @@ import time
 
 config_path = {}
 
+def get_file_name(config_path):
+    print('*****  get_file_name  ******')
+    #datetime.utcnow().strftime("%Y%m%dT%H%M%S")
+    filename = datetime.utcnow()
+    create_dir_if_doesnt_exist(config_path['log_file_path'])
+    files = glob.glob(config_path['log_file_path'] + '*.txt', recursive=True)
+    print('files',files)
+    max_file = None
+    if files:
+        max_path = max(files, key=os.path.getctime)
+        max_file = max_path.split('/')
+        print('max_file', max_file)
+        interval_time = datetime.strptime(max_file[-1], "%Y%m%dT%H%M%S.txt") - datetime.utcnow()
+        
+        return max_path
+    print('2', config_path['log_file_path'] + datetime.utcnow().strftime("%Y%m%dT%H%M%S.txt"))
+    return config_path['log_file_path'] + datetime.utcnow().strftime("%Y%m%dT%H%M%S.txt")
+    
 
-def move_files(source, destination):
+async def write_log(message, config_path):
+    print('in write log')
+    path = get_file_name(config_path=config_path)
+    print('path', path)
+    async with aiofiles.open(path, "ab+") as new_log:
+        print('write log file ')
+        await new_log.write(bytes(message, 'utf-8'))
+        await new_log.write(bytes('\n', 'utf-8'))
+    
+
+def log_message(source, destination, archive_path, config_path):
+    a =  config_path['log_message']['from'] + ' : '\
+                + source + '\n'\
+                + config_path['log_message']['to'] +' : ' \
+                + destination + '\n'\
+                + config_path['log_message']['archive']+ " : " \
+                + archive_path+ '\n'\
+                + "================================================================="
+    print('a', a)
+    return a
+
+
+
+def move_files(source, destination, archive_path):
+    shutil.copyfile( source,archive_path)
     os.replace(source, destination)
+    
 
 async def write_json_file(data, file_name):
+    print("** write json ***")
     async with aiofiles.open(file_name, "w") as new_json:
         await new_json.write(json.dumps(data, indent = 4))
     
 
 def create_dir_if_doesnt_exist(path):
+    print("** create path ***")
     if not os.path.exists(path):
                 os.mkdir(path)
 
@@ -32,16 +79,38 @@ async def read_all_xmls(xml_file_path, config_path):
             data = xmltodict.parse(await xml_file.read())
             cur_file_name = file.split('/')[-1]
             create_dir_if_doesnt_exist(config_path['output_json_file_path'])
+            
             file_name = cur_file_name[:-4] + str(datetime.utcnow()) + '.json'
             output_file = config_path['output_json_file_path'] + file_name
             await write_json_file(data=data, file_name=output_file)
+            
             create_dir_if_doesnt_exist(config_path['old_xml_path'])
-            move_files(source=file, destination= config_path['old_xml_path'] + cur_file_name)
+            destination = config_path['old_xml_path'] + cur_file_name
+
+            create_dir_if_doesnt_exist(config_path['xml_archive_path'])
+            archive_path = config_path['xml_archive_path'] + cur_file_name
+            print("move ")
+            move_files(
+                source=file, 
+                destination=destination,
+                archive_path=archive_path
+            )
+            print("log msg")
+            message = log_message(
+                source=file,
+                destination=destination,
+                archive_path=archive_path,
+                config_path=config_path
+            )
+            print("file name")
+            log_file = get_file_name(config_path=config_path)
+            print("write log")
+            await write_log(message=message, config_path=config_path)
 
 
 
 async def read_path(config_path):
-
+    print("** open read path ***")
     if config_path['input_xml_file_path']:
         xml_file_path = config_path['input_xml_file_path']
         if os.path.exists(path=xml_file_path):
@@ -53,6 +122,7 @@ async def read_path(config_path):
     
 
 async def load_config_file():
+    print("** open config file ***")
     with open('config.json', 'r') as cj:
             return json.load(cj)
     
